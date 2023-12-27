a13d1cb37a14d8ac616492acbc9020ec5a5b9627
Antonio <OBT59086@educastur.es>
2023-12-27 21:57:36 +0100

    Primera semana de trabajo.

--- Prueba_Git_Antonio/src/main/java/principal/Principal.java
+++ Prueba_Git_Antonio/src/main/java/principal/Principal.java
@@ -5,6 +5,16 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Empiezo mi programa dificilisimo");
+		System.out.println("Sigo con mi programa dificilisimo");
+		System.out.println("Programo mi programa dificilisimo");
+		System.out.println("Estoy contento con mi programa dificilisimo");
+		
+		
+		
+		
+		
+		
 	}
 
 }
