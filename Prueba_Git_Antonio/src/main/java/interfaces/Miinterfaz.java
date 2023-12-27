e0b825082865be74495215cdd74690e97f2e3128
Antonio <OBT59086@educastur.es>
2023-12-27 21:51:15 +0100

    Interfaces y modelo.

---
+++ Prueba_Git_Antonio/src/main/java/interfaces/Miinterfaz.java
@@ -0,0 +1,20 @@
+package interfaces;
+
+public interface Miinterfaz {
+
+	public int metododevuelveentero();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Prueba_Git_Antonio/src/main/java/modelo/MicClase.java
@@ -0,0 +1,13 @@
+package modelo;
+
+import interfaces.Miinterfaz;
+
+public class MicClase implements Miinterfaz{
+
+	@Override
+	public int metododevuelveentero() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	 
+}
